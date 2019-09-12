b95ee6dfa6f1b417725136ee39c0e4a132eefa40
Matthew Sanabria <msanabria@hashicorp.com>
2019-09-12 01:39:30 -0400

    Fixing logic errors

--- src/tf_fmt.sh
+++ src/tf_fmt.sh
@@ -33,28 +33,31 @@ ${OUTPUT}
 \`\`\`
 "
   else
-    if [ "${GITHUB_EVENT_NAME}" == "pull_request" ]; then
-      COMMENT=""
-      for file in ${OUTPUT}; do
-        FILE_DIFF=$(terraform fmt -no-color -write=false -diff "${file}" | sed -n '/@@.*/,//{/@@.*/d;p}')
-        COMMENT="${COMMENT}
+    # Otherwise the output will contain a list of unformatted filenames.
+    # Iterate through each file and build up a comment containing the diff
+    # of each file.
+    COMMENT=""
+    for file in ${OUTPUT}; do
+      FILE_DIFF=$(terraform fmt -no-color -write=false -diff "${file}" | sed -n '/@@.*/,//{/@@.*/d;p}')
+      COMMENT="${COMMENT}
 <details><summary><code>${file}</code></summary>
 \`\`\`diff
 ${FILE_DIFF}
 \`\`\`
 </details>
 "
-      done
+    done
+  fi
 
-      COMMENT_WRAPPER="#### \`terraform fmt\` Failed
+  if [ "${GITHUB_EVENT_NAME}" == "pull_request" ]; then
+    COMMENT_WRAPPER="#### \`terraform fmt\` Failed
 ${COMMENT}
 *Workflow: \`${GITHUB_WORKFLOW}\`, Action: \`${GITHUB_ACTION}\`*
 "
-      PAYLOAD=$(echo '{}' | jq --arg body "${COMMENT_WRAPPER}" '.body = $body')
-      COMMENTS_URL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
-      curl -s -S --header "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data "${PAYLOAD}" "${COMMENTS_URL}" > /dev/null
+    PAYLOAD=$(echo '{}' | jq --arg body "${COMMENT_WRAPPER}" '.body = $body')
+    COMMENTS_URL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
+    curl -s -S --header "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data "${PAYLOAD}" "${COMMENTS_URL}" > /dev/null
 
-      exit ${SUCCESS}
-    fi
+    exit ${SUCCESS}
   fi
 }
