c181a8314e90976194a1fdbc9ff7b20a9f84128c
Matthew Sanabria <msanabria@hashicorp.com>
2019-09-12 01:28:49 -0400

    Reintroducing existing fmt script

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,15 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 scriptDir=$(dirname ${0})
 
 source ${scriptDir}/tf_fmt.sh
 
-if [ "${INPUT_TERRAFORM_VERSION}" != "" ]; then
-  tfVersion=${INPUT_TERRAFORM_VERSION}
-else
-  echo "Input terraform_version cannot be empty"
-  exit 1
-fi
+function parseInputs {
+ 
+  # Required inputs
+  if [ "${INPUT_TERRAFORM_VERSION}" != "" ]; then
+    tfVersion=${INPUT_TERRAFORM_VERSION}
+  else
+    echo "Input terraform_version cannot be empty"
+    exit 1
+  fi
+
+  if [ "${INPUT_TERRAFORM_SUBCOMMAND}" != "" ]; then
+    tfSubcommand=${INPUT_TERRAFORM_SUBCOMMAND}
+  else
+    echo "Input terraform_subcommand cannot be empty"
+    exit 1
+  fi
+ 
+  # Optional inputs
+  tfWorkingDir=${INPUT_TERRAFORM_WORKING_DIR}
+}
+
 
 function installTerraform {
   url="https://releases.hashicorp.com/terraform/${tfVersion}/terraform_${tfVersion}_linux_amd64.zip"
@@ -17,22 +32,32 @@ function installTerraform {
   curl -s -L -o /tmp/terraform_${tfVersion} ${url}
   if [ "${?}" -ne 0 ]; then
     echo "Failed to download Terraform v${tfVersion}"
+    exit 1
   fi
   unzip -d /usr/local/bin /tmp/terraform_${tfVersion}
   if [ "${?}" -ne 0 ]; then
     echo "Failed to unzip Terraform v${tfVersion}"
+    exit 1
   fi
 }
 
-function debug {
+# function debug {
 
-  pwd
-  ls -la
-  printenv
+#   pwd
+#   ls -la
+#   printenv
 
-  cat ${GITHUB_EVENT_PATH}
-}
+#   cat ${GITHUB_EVENT_PATH}
+# }
 
-installTerraform
-terraformFmt
-debug
+parseInputs
+
+case "${tfSubcommand}" in
+  fmt)
+    installTerraform
+    terraformFmt
+    ;;
+  *)
+    echo "Error: Must provide a valid value for terraform_subcommand"
+    ;;
+esac
--- src/tf_fmt.sh
+++ src/tf_fmt.sh
@@ -1,7 +1,60 @@
 #!/bin/sh
 
 function terraformFmt {
-  OUTPUT=$(terraform fmt -no-color -check -list -recursive 2>&1)
+
+  set -e
+  if [ "${tfWorkingDir}" == "" ] || [ "${tfWorkingDir}" == "" ]; then
+    cd ${GITHUB_WORKSPACE}
+  else
+    cd ${GITHUB_WORKSPACE}/${tfWorkingDir}
+  fi
+
+  set +e
+  # OUTPUT=$(sh -c "terraform fmt -no-color -check -list -recursive $*" 2>&1)
+  OUTPUT=$(terraform fmt -no-color -check -list -recursive)
   SUCCESS=${?}
   echo "${OUTPUT}"
+  set -e
+
+  if [ "${SUCCESS}" -eq 0 ]; then
+      exit 0
+  fi
+
+  if [ "$TF_ACTION_COMMENT" = "1" ] || [ "$TF_ACTION_COMMENT" = "false" ]; then
+      exit $SUCCESS
+  fi
+
+  if [ "${SUCCESS}" -eq 2 ]; then
+    # If it exits with 2, then there was a parse error and the command won't have
+    # printed out the files that have failed. In this case we comment back with the
+    # whole parse error.
+    COMMENT="\`\`\`
+${OUTPUT}
+\`\`\`
+"
+  else
+    if [ "${GITHUB_EVENT_NAME}" == "pull_request" ]; then
+      COMMENT=""
+      for file in ${OUTPUT}; do
+        FILE_DIFF=$(terraform fmt -no-color -write=false -diff "${file}" | sed -n '/@@.*/,//{/@@.*/d;p}')
+        COMMENT="${COMMENT}
+<details><summary><code>${file}</code></summary>
+\`\`\`diff
+${FILE_DIFF}
+\`\`\`
+</details>
+"
+      done
+
+      COMMENT_WRAPPER="#### \`terraform fmt\` Failed
+${COMMENT}
+*Workflow: \`${GITHUB_WORKFLOW}\`, Action: \`${GITHUB_ACTION}\`*
+"
+      PAYLOAD=$(echo '{}' | jq --arg body "${COMMENT_WRAPPER}" '.body = $body')
+      COMMENTS_URL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
+      curl -s -S --header "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data "${PAYLOAD}" "${COMMENTS_URL}" > /dev/null
+
+      exit ${SUCCESS}
+    fi
+  fi
 }
